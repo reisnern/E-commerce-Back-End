b23fab4584bcdfdac0aad4a588dd69325bb909d1
Nick Reisner <nreisne@wgu.edu>
2021-06-12 16:33:07 -0500

    adding models

---
+++ models/producttag.js
@@ -0,0 +1,43 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+
+// import database connection from connection.js
+const sequelize = require('../config/connection');
+
+// initialize ProductTag model (table) by extending off Sequelize's Model class
+class ProductTag extends Model {}
+
+// set up fields and rules for ProductTag model
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag'
+  }
+);
+
+module.exports = ProductTag;
